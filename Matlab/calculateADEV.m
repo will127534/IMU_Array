501c3204e97d0fbe0cac751fe6fcbd527d745519
U-WILL-PC\will1 <will3509111@gmail.com>
2024-08-02 15:44:03 -0700

    Update code

--- Matlab/calculateADEV.m
+++ Matlab/calculateADEV.m
@@ -2,6 +2,7 @@
 close all;
 clear all;
 
+%% Load Data
 load("imu_data_2.mat")
 
 % Define the number of IMUs and data types
@@ -17,27 +18,8 @@ dataStruct.rate = fs; % Assuming the data rate is 1 Hz for this example
 % Define tau values for Allan Deviation calculation
 tau = floor(logspace(0, log10(maxSamples), 1000)) *0.01;
 
+%% Gyroscope Allan Deviation
 figure;
-
-% Calculate the average gyroscope values
-avgGyroX = (mean(squeeze(data(1:8, 6, :)), 1) + mean(squeeze(data(9:16, 6, :)), 1) - mean(squeeze(data(17:27, 6, :)), 1) - mean(squeeze(data(25:32, 6, :)), 1)) ./4; 
-avgGyroY = (mean(squeeze(data(1:8, 7, :)), 1) - mean(squeeze(data(9:16, 7, :)), 1) - mean(squeeze(data(17:27, 7, :)), 1) + mean(squeeze(data(25:32, 7, :)), 1)) ./4; 
-avgGyroZ = mean(squeeze(data(:, 8, :)), 1);
-
-
-dataStruct.freq = avgGyroX * 3600;
-[adevGyroX, ~, ~, tauUsed] = allan(dataStruct, tau, 'Average Gyro_X', 0);
-
-dataStruct.freq = avgGyroY * 3600;
-[adevGyroY, ~, ~, ~] = allan(dataStruct, tau, 'Average Gyro_Y', 0);
-
-dataStruct.freq = avgGyroZ * 3600;
-[adevGyroZ, ~, ~, ~] = allan(dataStruct, tau, 'Average Gyro_Z', 0);
-
-loglog(tauUsed, sqrt(adevGyroX), 'r', 'DisplayName', 'Avg Gyro_X (dps)','LineWidth',1);
-hold on;
-loglog(tauUsed, sqrt(adevGyroY), 'b', 'DisplayName', 'Avg Gyro_Y (dps)','LineWidth',1);
-loglog(tauUsed, sqrt(adevGyroZ), 'k', 'DisplayName', 'Avg Gyro_Z (dps)','LineWidth',1);
 
 for imuIndex = 1:numIMUs
     % Prepare data structure for root Allan deviation
@@ -54,6 +36,7 @@ for imuIndex = 1:numIMUs
     
     % Plot Allan Deviation
     loglog(tauUsed, sqrt(adevGyroX), 'DisplayName', 'Gyro_X (dps)','Color',"#D95319");
+    hold on;
     loglog(tauUsed, sqrt(adevGyroY), 'DisplayName', 'Gyro_Y (dps)','Color',"#4DBEEE");
     loglog(tauUsed, sqrt(adevGyroZ)', 'DisplayName', 'Gyro_Z (dps)','Color',"#A6A6A6");
     
@@ -63,33 +46,30 @@ for imuIndex = 1:numIMUs
     %legend('show');
 end
 
+% Calculate the average gyroscope values
+avgGyroX = (mean(squeeze(data(1:8, 6, :)), 1) + mean(squeeze(data(9:16, 6, :)), 1) - mean(squeeze(data(17:27, 6, :)), 1) - mean(squeeze(data(25:32, 6, :)), 1)) ./4; 
+avgGyroY = (mean(squeeze(data(1:8, 7, :)), 1) - mean(squeeze(data(9:16, 7, :)), 1) - mean(squeeze(data(17:27, 7, :)), 1) + mean(squeeze(data(25:32, 7, :)), 1)) ./4; 
+avgGyroZ = mean(squeeze(data(:, 8, :)), 1);
+
+dataStruct.freq = avgGyroX * 3600;
+[adevGyroX, ~, ~, tauUsed] = allan(dataStruct, tau, 'Average Gyro_X', 0);
+
+dataStruct.freq = avgGyroY * 3600;
+[adevGyroY, ~, ~, ~] = allan(dataStruct, tau, 'Average Gyro_Y', 0);
+
+dataStruct.freq = avgGyroZ * 3600;
+[adevGyroZ, ~, ~, ~] = allan(dataStruct, tau, 'Average Gyro_Z', 0);
+
+loglog(tauUsed, sqrt(adevGyroX), 'r', 'DisplayName', 'Avg Gyro_X (dps)','LineWidth',1);
+loglog(tauUsed, sqrt(adevGyroY), 'b', 'DisplayName', 'Avg Gyro_Y (dps)','LineWidth',1);
+loglog(tauUsed, sqrt(adevGyroZ), 'k', 'DisplayName', 'Avg Gyro_Z (dps)','LineWidth',1);
+
 xlim([tauUsed(1),tauUsed(end)])
 print(gcf, 'Gyroscope_Allan_Deviation.jpg', '-djpeg', '-r150');
 
+%% Accelerometer Allan Deviation
 
 figure;
-% Calculate the average accelerometer values
-avgAccX = (mean(squeeze(data(1:8, 3, :)), 1) + mean(squeeze(data(9:16, 3, :)), 1) - mean(squeeze(data(17:24, 3, :)), 1) - mean(squeeze(data(25:32, 3, :)), 1)) ./4; 
-avgAccY = (mean(squeeze(data(1:8, 4, :)), 1) - mean(squeeze(data(9:16, 4, :)), 1) - mean(squeeze(data(17:24, 4, :)), 1) + mean(squeeze(data(25:32, 4, :)), 1)) ./4; 
-avgAccZ = mean(squeeze(data(:, 5, :)), 1);
-
-
-
-
-dataStruct.freq = avgAccX;
-[adevAccX, ~, ~, tauUsed] = allan(dataStruct, tau, 'Average Acc_X ', 0);
-
-dataStruct.freq = avgAccY;
-[adevAccY, ~, ~, ~] = allan(dataStruct, tau, 'Average Acc_Y', 0);
-
-dataStruct.freq = avgAccZ;
-[adevAccZ, ~, ~, ~] = allan(dataStruct, tau, 'Average Acc_Z', 0);
-
-loglog(tauUsed, (adevAccX), 'r', 'DisplayName', 'Avg Acc_X  (dps)','LineWidth',1);
-hold on;
-loglog(tauUsed, (adevAccY), 'b', 'DisplayName', 'Avg Acc_Y  (dps)','LineWidth',1);
-loglog(tauUsed, (adevAccZ), 'k', 'DisplayName', 'Avg Acc_Z  (dps)','LineWidth',1);
-
 for imuIndex = 1:numIMUs
     % Prepare data structure for root Allan deviation
     dataStruct.rate = fs;
@@ -105,6 +85,7 @@ for imuIndex = 1:numIMUs
     
     % Plot Allan Deviation
     loglog(tauUsed, (adevAccX), 'DisplayName', 'Acc_X (mg)','Color',"#D95319");
+    hold on;
     loglog(tauUsed, (adevAccY), 'DisplayName', 'Acc_Y (mg)','Color',"#4DBEEE");
     loglog(tauUsed, (adevAccZ)', 'DisplayName', 'Acc_Z (mg)','Color',"#A6A6A6");
     
@@ -113,6 +94,25 @@ for imuIndex = 1:numIMUs
     title(['32x IMU Acc Allan Deviation (mg)']);
     %legend('show');
 end
+
+% Calculate the average accelerometer values
+avgAccX = (mean(squeeze(data(1:8, 3, :)), 1) + mean(squeeze(data(9:16, 3, :)), 1) - mean(squeeze(data(17:24, 3, :)), 1) - mean(squeeze(data(25:32, 3, :)), 1)) ./4; 
+avgAccY = (mean(squeeze(data(1:8, 4, :)), 1) - mean(squeeze(data(9:16, 4, :)), 1) - mean(squeeze(data(17:24, 4, :)), 1) + mean(squeeze(data(25:32, 4, :)), 1)) ./4; 
+avgAccZ = mean(squeeze(data(:, 5, :)), 1);
+
+dataStruct.freq = avgAccX;
+[adevAccX, ~, ~, tauUsed] = allan(dataStruct, tau, 'Average Acc_X ', 0);
+
+dataStruct.freq = avgAccY;
+[adevAccY, ~, ~, ~] = allan(dataStruct, tau, 'Average Acc_Y', 0);
+
+dataStruct.freq = avgAccZ;
+[adevAccZ, ~, ~, ~] = allan(dataStruct, tau, 'Average Acc_Z', 0);
+
+loglog(tauUsed, (adevAccX), 'r', 'DisplayName', 'Avg Acc_X  (mg)','LineWidth',1);
+loglog(tauUsed, (adevAccY), 'b', 'DisplayName', 'Avg Acc_Y  (mg)','LineWidth',1);
+loglog(tauUsed, (adevAccZ), 'k', 'DisplayName', 'Avg Acc_Z  (mg)','LineWidth',1);
+
 xlim([tauUsed(1),tauUsed(end)])
 print(gcf, 'Accelerometer_Allan_Deviation.jpg', '-djpeg', '-r150');
 
